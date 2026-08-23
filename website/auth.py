33ea5005e9a5e09af33dbb6062ea9d486779691d
Abip-82 <abipmahat04@gmail.com>
2026-08-23 08:58:59 +0545

    login and sign up pages

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,16 @@
-from flask import Blueprint , render_template
+from flask import Blueprint , render_template 
+import request
 
 auth = Blueprint("auth",__name__)
 
-@auth.route("/login")
+@auth.route("/login", methods = ['GET','POST'])
 def login():
     return render_template("login.html")
 
-@auth.route("/signup")
+@auth.route("/signup", methods = ['GET', 'POST'])
 def signup():
+    email = request.form.get("email")
+    username=request.form.get("username")
+    password = request.form.get("password")
+    password_confirm =  request.form.get("password_confirm")
     return render_template("signup.html")

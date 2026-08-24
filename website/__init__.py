601cc2b60d6505aa96a1fc0cdfc741b854063041
Abip-82 <abipmahat04@gmail.com>
2026-08-24 12:00:30 +0545

    sqlalchemy tests

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
+db = SQLAlchemy()
 
 def create_app():
     app =  Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    db.init_app(app)
+
+    from .models import User
+    
+    with app.app_context():
+        db.create_all()
 
     from .views import views
     from .auth import auth
---
+++ website/models.py
@@ -0,0 +1,7 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(50), nullable = False)
+    username = db.Column(db.String(100), nullable=False, unique = True)
+    password = db.Column(db.String(200), nullable=False)

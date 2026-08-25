ad7957e95ec3481fb4c23f3da54f73fcd0fdcea1
Abip-82 <abipmahat04@gmail.com>
2026-08-25 21:44:49 +0545

    working sign up adn login with db

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,9 @@
 from flask import Blueprint , render_template , redirect, url_for
 from .forms import SignupForm , LoginForm
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 auth = Blueprint("auth",__name__)
 
@@ -10,12 +14,11 @@ def login():
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
-
-        #check
-        #assuming pass:
-           # return redirect(url_for(views.home))
-        form.form_errors = ["Invalid email or password"]
-
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password_hashed, password):
+            return(redirect(url_for("views.home")))
+        else:
+            form.form_errors = ["Invalid email or password"]
     return render_template("login.html", form=form)
 
 @auth.route("/signup", methods = ['GET', 'POST'])
@@ -25,12 +28,13 @@ def signup():
 
     if form.validate_on_submit():
         email = form.email.data
-        username = form.username.data
         password = form.password.data
-        password_confirm = form.password_confirm.data
+        username = form.username.data
+        password_hashed = generate_password_hash(password)
 
-        #user account banauni
- 
+        user = User(email=email,username=username,password_hashed=password_hashed)
+        db.session.add(user)
+        db.session.commit()
         return redirect(url_for("auth.login"))
 
     return render_template("signup.html", form=form)

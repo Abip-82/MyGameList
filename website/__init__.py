d90e131ea6f20874aa1a3254a4f2d5d5ca9cc181
Abip-82 <abipmahat04@gmail.com>
2026-08-27 15:00:09 +0545

    LoginManager and userloader

--- website/__init__.py
+++ website/__init__.py
@@ -2,15 +2,18 @@ from flask import Flask
 import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 load_dotenv()
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app =  Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .models import User
     
@@ -24,3 +27,9 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/auth")
 
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+    return User.query.get(int(user_id))
+

b6df1ef1b03e107165d15915e7c5a8a35c27e77f
Abip-82 <abipmahat04@gmail.com>
2026-08-29 17:23:22 +0545

    logout + authentication MVP

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,7 @@ from .forms import SignupForm , LoginForm
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user 
+from flask_login import login_user , logout_user
 
 
 
@@ -41,6 +41,11 @@ def signup():
         return redirect(url_for("auth.login"))
 
     return render_template("signup.html", form=form)
+
+@auth.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
 
 
 
